1b7887013d0cdab95b9e676232a08ea676446135
kf2304072 <cometrue.6398@gmail.com>
2023-08-20 23:44:11 +0900

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [todoTitle, setTodoTitle] = useState('')
+  const [todoId, setTodoId] = useState(todos.length + 1)
+  const handleAddFormChanges = (e) => {
+    setTodoTitle(e.target.value)
+  }
+  const handleAddTodo = () => {
+    setTodos([...todos, { id: todoId, title: todoTitle }])
+    setTodoId(todoId + 1)
+    setTodoTitle('')
+  }
+  // 問題2. 関数に削除対象のtodoが渡っているか確認しよう
+  const handleDeleteTodo = (targetTodo) => {
+    console.log(targetTodo)
+    //ここまで
+    // 問題3. 削除ボタンを押した時の処理を書こう
+    setTodos(todos.filter((todo) => todo !== targetTodo))
+
+    // ここまで
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <input
+          type="text"
+          label="タイトル"
+          value={todoTitle}
+          onChange={handleAddFormChanges}
+        />
+        <button onClick={handleAddTodo}>作成</button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span>{todo.title}</span>
+            {/* 問題1. 削除ボタンを設置しよう */}
+            <button onClick={() => handleDeleteTodo(todo)}>削除</button>
+            {/* ここまで */}
+          </li>
+        ))}
+      </ul>
+    </>
+
   );
 }
 
